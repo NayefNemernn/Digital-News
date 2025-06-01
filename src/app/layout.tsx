db9349dff84cbe0ad03f87ba371401a434e13902
NayefNemernn <nayefnemernn@gmail.com>
2025-06-01 08:39:28 +0300

    Completing the Header

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,14 +1,17 @@
+
+
+import 'bootstrap-icons/font/bootstrap-icons.css';
+import 'bootstrap/dist/css/bootstrap.css';
+import 'aos/dist/aos.css'
+
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import { EB_Garamond } from "next/font/google";
+
+import Header from '@/components/Header';
+import "./variables.css";
 import "./globals.css";
-
-const geistSans = Geist({
+const ebGaramond = EB_Garamond({
   variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
   subsets: ["latin"],
 });
 
@@ -24,7 +27,8 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={`${geistSans.variable} ${geistMono.variable}`}>
+      <body className={`${ebGaramond.className}`}>
+        <Header />
         {children}
       </body>
     </html>
---
+++ src/components/Header.tsx
@@ -0,0 +1,37 @@
+'use client';
+
+import React, {useState} from 'react';
+import './header.css';
+import Nav from './Nav';
+import Sci from './Sci';
+import SearchForm from './SearchForm';
+
+export default function Header() {
+
+  const [open, setopen] = useState(false);
+
+  const handleFormOpen = (e:Event | any )=>{
+    e.preventDefault();
+    setopen(!open);
+  }
+
+  return (
+    <header id="header" 
+    className='header d-flex align-items-center fixed-top'>
+        <div className="container-fluid container-xl d-flex align-items-center justify-content-between">
+        <a href="/" className="logo d-flex align-items-center">
+            <h1>Digital News</h1>
+            </a>
+            </div>  
+            <Nav/>
+            <div className="position-relative">
+              <Sci />
+              <a className='mx-2 js-search-open' onClick={handleFormOpen}>
+                <span className='bi-search'></span>
+              </a>
+              <SearchForm  active={open} formOpen={handleFormOpen}/>
+            </div>
+
+    </header>
+  )
+}
